b6e03732dabd1f5c2388884a9d6e7ea2d5463ced
AntonW <awiklund76@gmail.com>
2024-04-01 14:20:31 +0300

    Add news classification using TF-IDF vectorization and prediction

--- nlp_enriched_news.py
+++ nlp_enriched_news.py
@@ -1,10 +1,15 @@
 import spacy
 import pandas as pd
 from tqdm import tqdm
+import joblib
 
 print()
 
 nlp = spacy.load('en_core_web_lg')
+
+classifier = joblib.load('./models/news_classifier.pkl')
+vectorizer = joblib.load('./models/tfidf_vectorizer.pkl')
+
 
 data = pd.read_csv('./data/news.csv')
 
@@ -19,11 +24,21 @@ tqdm.pandas()
 print("Extracting entities from headlines...")
 data['entities'] = data['headline'].progress_apply(extract_entities)
 
+
+
 print("Extracting entities from bodies...")
 data['entities'] += data['body'].progress_apply(extract_entities)
 
+# Assuming you want to predict the category based on the 'body'. Adjust if necessary.
+body_text = data['body'].tolist()
+texts_vect = vectorizer.transform(body_text)  # Transform the texts to TF-IDF vector
+predicted_categories = classifier.predict(texts_vect)  # Predict the categories
+
+# Add the predicted categories to your DataFrame
+data['predicted_category'] = predicted_categories
+
 # put the entities to the left of the DataFrame
-data = data[['entities', 'headline', 'link', 'date', 'body']]
+data = data[['predicted_category','entities', 'headline', 'link', 'date', 'body']]
 
 # Export the DataFrame to a .csv file
 try:
