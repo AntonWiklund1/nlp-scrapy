bdf7d05a83fdf0cd2a405e8e358f01ba5348c201
AntonWiklund1 <awiklund76@gmail.com>
2024-04-26 23:19:52 +0300

    Add plot_ud_ratios function to visualization module

--- vissualize/plot.py
+++ vissualize/plot.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import confusion_matrix
-
+import numpy as np
 
 def plot_confusion_matrix(actuals, predictions, classes, title='Confusion Matrix', file_name='./results/cm/confusion_matrix.png'):
     cm = confusion_matrix(actuals, predictions)
@@ -32,4 +32,20 @@ def plot_gradients(fc1_norms, fc2_norms, title, file_path):
     plt.legend()
     plt.savefig(file_path)
     plt.close()
+
+def plot_ud_ratios(ud_ratio_history, filename):
+    plt.figure(figsize=(20, 4))
+    for i, ud in enumerate(zip(*ud_ratio_history)):  # Transpose the list of lists
+        plt.plot(ud, label=f'weight param {i}')
+    plt.axhline(y=-3, color='k', linestyle='--', label='Target Ratio (-3)') # Horizontal line at log10(1e-3)
+
+    plt.xlabel('Training Step')
+    plt.ylabel('Log10(Update-to-Data Ratio)')
+    plt.title('Log-Scale Update-to-Data Ratios for Model Weights Over Training')
+    plt.legend(loc='upper left', bbox_to_anchor=(1, 1))
+    plt.tight_layout()
+    plt.savefig(filename, bbox_inches='tight')
+    plt.close()
+
+
 
